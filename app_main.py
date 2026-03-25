308e4f7d69e02e25acfdc6eb5ac2e1fb0a07deca
KG PN <karoginny@gmail.com>
2026-03-25 00:03:52 -0400

    ok

---
+++ app_main.py
@@ -0,0 +1,18 @@
+import sys
+from PyQt5.QtWidgets import QApplication # type: ignore
+from ui.theme import aplicar_tema
+from ui.login import Login
+from ui.main_window import MainWindow
+
+def abrir_main():
+    global janela
+    janela = MainWindow()
+    janela.show()
+
+app = QApplication(sys.argv)
+aplicar_tema(app)
+
+login = Login(abrir_main)
+login.show()
+
+sys.exit(app.exec_())
