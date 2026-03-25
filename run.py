02c7a501f36d0961ea83bf4b0138b94746d88018
KG PN <karoginny@gmail.com>
2026-03-25 00:07:20 -0400

    ok

--- run.py
+++ run.py
@@ -4,8 +4,8 @@ import os
 sistema = platform.system()
 
 if sistema == "Linux":
-    print("Modo console ativado")
+    print("Modo console")
     os.system("python console_main.py")
 else:
-    print("Modo interface ativado")
+    print("Modo interface")
     os.system("python app_main.py")
