4d6c6dc6ec93d1e4313234f561302d39ef652c6e
KG PN <karoginny@gmail.com>
2026-03-26 12:39:58 -0400

    ok

--- console_main.py
+++ console_main.py
@@ -1,16 +1,22 @@
 from core.optimizer import limpar_cache, liberar_ram
 
-while True:
-    print("\nAUREXA CONSOLE")
-    print("1 - Limpar Cache")
-    print("2 - Liberar RAM")
-    print("0 - Sair")
 
-    op = input("Escolha: ")
+def main():
+    while True:
+        print("\nAUREXA CONSOLE")
+        print("1 - Limpar Cache")
+        print("2 - Liberar RAM")
+        print("0 - Sair")
 
-    if op == "1":
-        print(limpar_cache())
-    elif op == "2":
-        print(liberar_ram())
-    elif op == "0":
-        break
+        op = input("Escolha: ")
+
+        if op == "1":
+            print(limpar_cache())
+        elif op == "2":
+            print(liberar_ram())
+        elif op == "0":
+            break
+
+
+if __name__ == "__main__":
+    main()
